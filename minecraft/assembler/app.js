b73aa5bb136a6df9fccc8bb8431becf30295e308
simon816 <commits@simon816.com>
2020-08-27 23:03:04 +0100

    Update assembler editor to new CBA version

--- minecraft/assembler/app.js
+++ minecraft/assembler/app.js
@@ -8,20 +8,23 @@
     editor.setTheme("ace/theme/chrome");
 
     var statusBar;
+    var errorConsole;
     var fileList;
-    var example = 'fib-cbl';
-
     var files = {};
     var fileOrdering = [];
-
     var activeFilename = null;
 
     function fixPanelWidth() {
         document.getElementById('editor').style.left = document.getElementById('file-panel').offsetWidth + 'px';
+        document.getElementById('console').style.left = document.getElementById('file-panel').offsetWidth + 'px';
     }
 
     document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('build-button').addEventListener('click', doBuild);
+        document.getElementById('reset-button').addEventListener('click', resetState);
+        document.getElementById('eg-select').addEventListener('change', exampleSelect);
+        document.getElementById('new-file-btn').addEventListener('click', newFile);
+        document.getElementById('eg-select').value = 'none';
 
         statusBar = document.getElementById('status-bar');
         statusBar.textContent = 'Ready';
@@ -29,12 +32,14 @@
 
         fileList = document.getElementById('file-list');
 
-        document.getElementById('eg-select').addEventListener('change', setExample);
-        document.getElementById('eg-select').value = example;
+        errorConsole = document.getElementById('error-console');
 
-        document.getElementById('new-file-btn').addEventListener('click', newFile);
+        loadOrCreateState();
+    });
 
-        resetSession();
+    window.addEventListener('beforeunload', function() {
+        saveState();
+        return true;
     });
 
     var sampleFibCode = {
@@ -50,8 +55,8 @@
             +'    text_append $msg, $x\n    text_send $msg, $all_players\n    set_command_block :post_tick\n\n    post_tick:\n    clear_command_block\n'
             +'    $counter += 1\n    $old_x = $x\n    $x = $y\n    $y += $old_x\n    rangebr $x, 0, NULL, :loop, :end\n\n    end:\n    ret\n}\n',
         cbl: 'include "Text"\ninclude "Game"\n\nint x;\nint y;\nint counter;\nint old_x;\n\nasync void main() {\n    x = 0;\n    y = 1;\n'
-            +'    counter = 1;\n    do {\n        Text t;\n        t << "fib(" << counter++ << ") = " << x;\n        t.send_to_all();\n'
-            +'        await Game.tick();\n        old_x = x;\n        x = y;\n        y += old_x;\n    } while(x >= 0);\n}\n'
+            +'    counter = 1;\n\    do {\n        Text t;\n        t << "fib(";\n        t.append_ref(counter);\n        t << ") = ";\n        t.append_ref(x);\n'
+            +'        t.send_to_all();\n        counter++;\n        await Game.tick();\n        old_x = x;\n        x = y;\n        y += old_x;\n    } while(x >= 0);\n}'
     };
 
     var fibDpd = { name: 'fib.dpd', value: '[Datapack]\nnamespace = fib\nplace location = 0 56 0\nspawn location = ~ ~2 ~\n' };
@@ -59,25 +64,20 @@
         'fib-asm': [ { name: 'fib.asm', value: sampleFibCode.asm }, fibDpd ],
         'fib-c': [ { name: 'fib.c', value: sampleFibCode.c }, fibDpd ],
         'fib-ir': [ { name: 'fib.ir', value: sampleFibCode.ir }, fibDpd ],
-        'fib-cbl': [ { name: 'fib.cmdl', value: sampleFibCode.cbl }, fibDpd ],
+        'fib-cbl': [ { name: 'fib.cbl', value: sampleFibCode.cbl }, fibDpd ],
     };
 
-    function setExample(event) {
-        example = event.target.value;
-        if (example !== 'none') {
-            resetSession();
+    function exampleSelect(event) {
+        var example = event.target.value;
+        if (example === 'none' || (fileOrdering.length && !confirm("Clear files and load example code?"))) {
+            return;
         }
+        setExample(example);
     }
 
-    function resetSession() {
-        fileOrdering = [];
-        activeFilename = null;
-        for (var oldFile in files) {
-            fileList.removeChild(files[oldFile].domNode);
-        }
-        files = {};
-        setFileActive(-1);
+    function setExample(example) {
         if (example in examples) {
+            resetState();
             var eg = examples[example];
             for (var i = 0; i < eg.length; i++) {
                 var file = eg[i];
@@ -85,6 +85,47 @@
             }
             setFileActive(0);
         }
+    }
+
+    function loadOrCreateState() {
+        var state = window.localStorage.getItem('assemblerState');
+        if (state) {
+            state = JSON.parse(state);
+            for (var i = 0; i < state.fileOrdering.length; i++) {
+                var file = state.fileOrdering[i];
+                addFile(file, state.fileContent[file]);
+            }
+            setFilenameActive(state.activeFilename);
+        } else {
+            setExample('fib-cbl');
+            saveState();
+        }
+    }
+
+    function resetState() {
+        for (var oldFile in files) {
+            fileList.removeChild(files[oldFile].domNode);
+        }
+        fileOrdering = [];
+        activeFilename = null;
+        files = {};
+        setFileActive(-1);
+    }
+
+    function saveState() {
+        var content = {};
+        for (var file in files) {
+            content[file] = files[file].content;
+        }
+        if (activeFilename !== null) {
+            content[activeFilename] = editor.getValue();
+        }
+        var state = {
+            fileOrdering: fileOrdering,
+            activeFilename: activeFilename,
+            fileContent: content,
+        };
+        window.localStorage.setItem('assemblerState', JSON.stringify(state));
     }
 
     function newFile() {
@@ -130,13 +171,17 @@
             if (event.target === deleteBtn) {
                 return;
             }
-            var idx = fileOrdering.indexOf(filename);
-            setFileActive(idx);
+            setFilenameActive(filename);
         });
         fileNode.appendChild(filenameNode);
         fileNode.appendChild(deleteBtn);
         fileList.appendChild(fileNode);
         setFileActive(fileOrdering.length - 1);
+    }
+
+    function setFilenameActive(filename) {
+        var idx = fileOrdering.indexOf(filename);
+        setFileActive(idx);
     }
 
     function setFileActive(idx) {
@@ -148,6 +193,8 @@
         if (idx < 0 || idx >= fileOrdering.length) {
             editor.getSession().setMode('ace/mode/text');
             setEditor('');
+            activeFilename = null;
+            saveState();
             return;
         }
         var filename = fileOrdering[idx];
@@ -156,6 +203,7 @@
         fileInfo.domNode.classList.add('active');
         editor.getSession().setMode(modeForFilename(filename));
         setEditor(fileInfo.content);
+        saveState();
     }
 
     function setEditor(value) {
@@ -168,7 +216,7 @@
         if (filename.endsWith('.asm')) {
             return 'ace/mode/assembly_x86';
         }
-        if (filename.endsWith('.c') || filename.endsWith('.cmdl')) {
+        if (filename.endsWith('.c') || filename.endsWith('.cbl')) {
             return 'ace/mode/c_cpp';
         }
         if (filename.endsWith('.dpd')) {
@@ -182,6 +230,7 @@
         if (activeFilename !== null) {
             files[activeFilename].content = editor.getValue();
         }
+        saveState();
 
         statusBar.textContent = 'Compiling...';
         statusBar.style.color = 'orange';
@@ -192,8 +241,10 @@
         ajax.postJSON('.', {'files': fileArg}, function(data) {
             if (data.error) {
                 statusBar.style.color = 'red';
-                statusBar.textContent = 'Error! ' + data.error;
+                statusBar.textContent = 'Error! See console below';
+                errorConsole.value = data.error;
             } else {
+                errorConsole.value = '';
                 statusBar.style.color = '#42ff42';
                 statusBar.textContent = 'Complete';
                 if (data.zip) {
@@ -218,7 +269,8 @@
             }
         }, function(error) {
             statusBar.style.color = 'red';
-            statusBar.textContent = 'Error! ' + error;
+            statusBar.textContent = 'Error! See console below';
+            errorConsole.value = error;
         });
     }
 
